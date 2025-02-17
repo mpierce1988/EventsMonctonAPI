bbb83fe57ccff9df8b3ea488745b5915e8506ba8
michaelpierce <michael.todd.pierce@gmail.com>
2025-02-17 17:25:34 -0400

    Created settings.py using Pydantic-settings, to read the .env file

---
+++ app/core/settings.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+    google_sheets_url: str = ''
+
+settings = Settings()

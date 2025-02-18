e5a8e71a064ac5f96f6cad4a1e5ec54b9d825176
michaelpierce <michael.todd.pierce@gmail.com>
2025-02-17 23:06:19 -0400

    Implemented unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db.database import Base, get_db
+
+# Use an in-memory SQLite database for testing
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create Test Fixtures, for use in other Pytest files
+@pytest.fixture(scope="module")
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture(scope="function")
+def db_session(test_db):
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
---
+++ tests/test_database.py
@@ -0,0 +1,32 @@
+from app.db.database import get_db
+from app.db.models import Event
+from datetime import datetime
+
+def test_get_db(db_session):
+    db = next(get_db())  # Gets next item from iterator (get_db() has a yield)
+    assert db is not None
+
+def test_create_event(db_session):
+    event_data = {
+        "datetime": datetime(2023, 10, 1),
+        "type": "Festival",
+        "information": "Chocolat Moncton: Hot Chocolate Festival",
+        "venue": "various",
+        "cost": "$140",
+        "link_type": "Website",
+        "link": "https://chocolatmoncton.com/"
+    }
+
+    event = Event(**event_data)
+    db_session.add(event)
+    db_session.commit()
+
+    stored_event = db_session.query(Event).filter(Event.information == event_data["information"]).first()
+    assert stored_event is not None
+    assert stored_event.type == event_data["type"]
+    assert stored_event.datetime == event_data["datetime"]
+    assert stored_event.venue == event_data["venue"]
+    assert stored_event.cost == event_data["cost"]
+    assert stored_event.link_type == event_data["link_type"]
+    assert stored_event.link == event_data["link"]
+    assert stored_event.id is not None

57f2c6d1c1b0357c0a528cabcd07fa1275d05c70
michaelpierce <michael.todd.pierce@gmail.com>
2025-02-18 00:24:00 -0400

    Added multiple locations for .env file, to handle cases where pytest is being run from either the root of the test folder

--- app/core/settings.py
+++ app/core/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=['.env', '../.env', '../../.env'], env_file_encoding='utf-8')
 
     google_sheets_url: str = ''
 
